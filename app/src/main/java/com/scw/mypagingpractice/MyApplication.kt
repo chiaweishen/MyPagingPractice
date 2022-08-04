dd12bc18ecd0bcf45de895d094d568d7de909767
ethanshen <ethan.scw@gmail.com>
2022-08-04 11:30:29 +0800

    Remove stetho

--- app/src/main/java/com/scw/mypagingpractice/MyApplication.kt
+++ app/src/main/java/com/scw/mypagingpractice/MyApplication.kt
@@ -1,7 +1,6 @@
 package com.scw.mypagingpractice
 
 import android.app.Application
-import com.facebook.stetho.Stetho
 import com.scw.mypagingpractice.di.module.*
 import com.scw.mypagingpractice.util.MyDebugTree
 import org.koin.android.ext.koin.androidContext
@@ -11,15 +10,8 @@ import timber.log.Timber
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        initStetho()
         initTimber()
         initKoin()
-    }
-
-    private fun initStetho() {
-        if (BuildConfig.DEBUG) {
-            Stetho.initializeWithDefaults(this)
-        }
     }
 
     private fun initTimber() {
